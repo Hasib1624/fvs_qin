22721142f39a91329075045c35f0424217bf6cf0
Hasib1624 <38067466+Hasib1624@users.noreply.github.com>
2020-11-29 15:01:10 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import numpy as np
+from copy import copy
 
-# from copy import copy
+import numpy as np
 
 
 class AdjNode:
@@ -72,57 +72,60 @@ class Graph:
         v = np.random.choice(len(self.FVS))
         # Find the first vertex it is neighbor with
         temp = self.graph[self.FVS[v][1].vertex]
-        print(self.FVS[v])
+        #         print(self.FVS[v])
         # Find its neighbors
         neighbor_L = []
         while temp:
-            print(temp)
+            #             print(temp)
             for i, node in enumerate(self.L):
-                print('\t', node)
+                #                 print('\t', node)
                 if node[1].vertex == temp.vertex:
-                    print("Neighbor found")
+                    #                     print("Neighbor found")
                     neighbor_L.append(i)
             temp = temp.next
+
+
 #         temp = self.FVS[v]
-        print(neighbor_L)
-        
+#         print(neighbor_L)
+
         neighbor_n = len(neighbor_L)
         if neighbor_n == 0:
-            print("First Case")
+            #             print("First Case")
             self.L.append(self.FVS[v])
             self.FVS.pop(v)
         elif neighbor_n == 1:
-            print("Second Case")
+            #             print("Second Case")
             self.L.insert(neighbor_L[0], self.FVS[v])
             self.FVS.pop(v)
         else:
-            print("Third Case")
+            #             print("Third Case")
             idx = max(neighbor_L)
             self.L.insert(neighbor_L.index(idx), self.FVS[v])
             self.FVS.pop(v)
-            
+
             conflict_l = self.get_conflict()
 
             if len(conflict_l) > 0:
                 for i, nd in enumerate(self.L):
                     if nd[1].vertex in conflict_l:
                         self.FVS.append(self.L[i])
-                        self.L.pop(i)      
+                        self.L.pop(i)
             else:
                 rnd = np.random.uniform()
                 if rnd <= np.exp(-(-1 + len(conflict_l)) / self.T):
                     for i, nd in enumerate(self.L):
-                        if nd.vertex in conflict_l:
+                        if nd[1].vertex in conflict_l:
                             self.FVS.append(self.L[i])
                             self.L.pop(i)
         self.print_status()
-        print()
-        
-        return self.FVS, self.get_energy()
+        #         print()
+
+        return copy(self.FVS), self.get_energy()
 
     # Print the graph
 
     def print_status(self):
+        return
         print("FVS : ", end='\t')
         for i, node in self.FVS:
             print(i, end=', ')
@@ -146,7 +149,6 @@ class Graph:
             print(node)
         print()
 
-
 if __name__ == "__main__":
     V = 6
 
@@ -158,18 +160,37 @@ if __name__ == "__main__":
                 continue
             if np.random.uniform() < 0.5:
                 graph.add_edge(i, j)
-    # graph.add_edge(0, 1)
-    # graph.add_edge(0, 2)
-    # graph.add_edge(0, 3)
-    # graph.add_edge(1, 2)
 
     graph.print_agraph()
-    graph.initialize()
 
-    N_t = 50
-    fvs_l = []
-    energy_l = []
-    for _ in range(N_t):
-        fvs, energy = graph.run()
-        fvs_l.append(fvs)
-        energy_l.append(energy)
+    alpha = 0.99
+    T = 0.6 + alpha
+
+    fvs_all = []
+    energy_all = []
+    min_energy = V
+
+    n_fail = 0
+    while n_fail < 10:
+        T -= alpha
+        graph.initialize(T)
+
+        N_t = 50
+        fvs_l = []
+        energy_l = []
+        for _ in range(N_t):
+            fvs, energy = graph.run()
+            fvs_l.append(fvs)
+            energy_l.append(energy)
+
+        if min(energy_l) < min_energy:
+            min_energy = min(energy_l)
+            n_fail = -1
+
+        n_fail += 1
+        fvs_all += fvs_l
+        energy_all += energy_l
+
+    print('FVS : ')
+    for vertex in fvs_all[energy_all.index(min(energy_all))]:
+        print(vertex[0], end=", ")
